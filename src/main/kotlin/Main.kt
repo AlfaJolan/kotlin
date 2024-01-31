c1e9822d41f18d2d3ea3c95858aa4ee38461922c
Nurkhan <31109@iitu.edu.kz>
2024-01-31 13:44:28 +0600

    Part 1, LAB 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun print(n: String) : String{
+    return "Hello, my name is " + n;
 }
+
+fun main() {
+     var myName: String = "Nurkhan Kuangaliyev"
+    println(print(myName))
+}
+
